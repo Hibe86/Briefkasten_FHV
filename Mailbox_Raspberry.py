d931542ed4214666cd7be88d2b2536990d25b5ac
Ben_H <102252386+Hibe86@users.noreply.github.com>
2022-04-15 20:30:56 +0200

    Add files via upload
    Ready to check with original parts

---
+++ Mailbox_Raspberry.py
@@ -0,0 +1,48 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+# I/O Pins
+btnMail = 16
+btnDoor = 20
+btnParcel = 21
+outpLED =  6
+
+# gerenal Variables
+
+bncTime = 5000 # time in ms
+
+# GPIO setup
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(btnMail, GPIO.IN)
+GPIO.setup(btnDoor, GPIO.IN)
+GPIO.setup(btnParcel, GPIO.IN)
+GPIO.setup(outpLED, GPIO.OUT)
+
+# Callback Function
+def callbackFunc(channel):
+    if channel == btnMail:
+        value = "Brieffach"
+    elif channel == btnDoor:
+        value = "Briefkastentür"
+    elif channel == btnParcel:
+        value = "Paketfach"
+    else:
+        raise Exception
+    
+    if GPIO.input(channel):
+        print("Empfangen " + value)
+    else:
+        print("entmneommen " + value)
+
+# Definition of Pins
+GPIO.add_event_detect(btnMail, GPIO.BOTH, callback = callbackFunc, bouncetime=bncTime)
+GPIO.add_event_detect(btnDoor, GPIO.BOTH, callback = callbackFunc, bouncetime=bncTime)
+GPIO.add_event_detect(btnParcel, GPIO.BOTH, callback = callbackFunc, bouncetime=bncTime)
+
+# Main programm
+while True:
+    try:
+        sleep(0.1)
+    except:
+        GPIO.cleanup()
