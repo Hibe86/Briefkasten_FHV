10c3fd31ab99250e09663cd6ab1aaec711c5f58d
Ben_H <102252386+Hibe86@users.noreply.github.com>
2022-04-25 23:16:22 +0200

    Update API_Mail.py
    API defined

--- API_Mail.py
+++ API_Mail.py
@@ -1,29 +1,86 @@
-def send_status(Sensor,Status):
-    import requests
-    import json
-    
-    sensor_id = Sensor 
-    api_key = "wing_test"
-    url = "http://domainname.org/api/sensors/" + Sensor   # todo: change the url -- "http://domainname.org/api/sensors/"
-    
-    # Post request
-    data={Sensor : Status}
-    headers = { 'Content-Type' : 'application/json', 'api_key' : api_key }
-    response = requests.post(url, data = json.dumps(data), headers = headers)
-    
-    return response.content
+# general imports
+from datetime import datetime
+import requests
+import json
 
-def send_error(Sensor, Errorcode):
-    import requests
-    import json
+# global variables/settings
+url_Mail = "https://briefkasten.azurewebsites.net/briefkasten/brief/post"
+url_Parcel = "https://briefkasten.azurewebsites.net/briefkasten/paket/post"
+url_Mail_Door = "https://briefkasten.azurewebsites.net/briefkasten/brieftuere/post"
+url_Parcel_Door = "https://briefkasten.azurewebsites.net/briefkasten/pakettuere/post"
+headers = {"Content-Type" : "application/json"}
+
+def getActualTime():
+    actTime = (datetime.now().strftime("%H:%M:%S")) # format Date Time to correct format
+    return actTime
+
+def getActualDate():
+    actDate = (datetime.now().strftime("%d-%m-%Y"))
+    return actDate
+
+# send status of mailbox to url
+def send_status_mail(Message_change,StatusMail,StatusMailDoor):
+    try:
+        data = {"status" : Message_change,
+                "brieffach" : StatusMail,
+                "time" : getActualTime(),
+                "date" : getActualDate(),
+                }
+        print("Send status Mail")
+    except:
+        raise ValueError("Wrong format of data to transmit")
+    try:
+        print("Send status Mail correct")
+        response = requests.post(url_Mail, data = json.dumps(data), headers = headers)
+    except:
+        raise ValueError("Wrong response")
     
-    sensor_id = Sensor 
-    api_key = "wing_test"
-    url = "http://domainname.org/api/sensors/" + Sensor   # todo: change the url -- "http://domainname.org/api/sensors/"
     
-    # Post request
-    data={Sensor : Errorcode}
-    headers = { 'Content-Type' : 'application/json', 'api_key' : api_key }
-    response = requests.post(url, data = json.dumps(data), headers = headers)
+# send status of parcel box to url
+def send_status_parcel(Message_change,StatusParcel,StatusParcelDoor):
+    try:
+        data = {"status" : Message_change,
+                "packetfach" : StatusParcel,
+                "time" : getActualTime(),
+                "date" : getActualDate(),
+                }
+        print("Send status parcel")
+    except:
+        raise ValueError("Wrong format of data to transmit")
+    try:
+        response = requests.post(url_Parcel, data = json.dumps(data), headers = headers)
+        print("Send status Parcel correct")
+    except:
+        raise ValueError("Wrong response")
     
-    return response.content
+def send_status_mail_Door(Message_change,StatusMailDoor):
+    try:
+        data = {"status" : Message_change,
+                "briefkastentuere" : StatusMailDoor,
+                "time" : getActualTime(),
+                "date" : getActualDate(),
+                }
+        print("Send status Mail")
+    except:
+        raise ValueError("Wrong format of data to transmit")
+    try:
+        print("Send status Mail correct")
+        response = requests.post(url_Mail_Door, data = json.dumps(data), headers = headers)
+    except:
+        raise ValueError("Wrong response")
+    
+def send_status_parcel_Door(Message_change,StatusParcelDoor):
+    try:
+        data = {"status" : Message_change,
+                "pakettuer" : StatusParcelDoor,
+                "time" : getActualTime(),
+                "date" : getActualDate(),
+                }
+        print("Send status Mail")
+    except:
+        raise ValueError("Wrong format of data to transmit")
+    try:
+        print("Send status Mail correct")
+        response = requests.post(url_Parcel_Door, data = json.dumps(data), headers = headers)
+    except:
+        raise ValueError("Wrong response")
